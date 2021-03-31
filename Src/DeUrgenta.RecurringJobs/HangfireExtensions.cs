69b4c51da6f3d56e95a5a3afe067f16d23ba0eaa
Alexandru Puiu Albu <matrixpuiu@yahoo.com>
2021-03-31 20:52:00 +0200

    Feature/hangfire project (#2)
    * Add initial project and examples of controllers
    
    * Configure mediatr, fix build
    
    * fix comments, use postgres, regenerate migration
    
    * Hangfire project with Docker file
    
    * Authenticated Dashboard access
    
    * Add editor config + cleanup
    
    * Rework a little bit recuring jobs
    
    Co-authored-by: Ion Dormenco <idormenco@gmail.com>
    Co-authored-by: Alex Albu <alex.albu@novanet.no>

---
+++ Src/DeUrgenta.RecurringJobs/HangfireExtensions.cs
@@ -0,0 +1,59 @@
+﻿using Hangfire;
+using Hangfire.Dashboard;
+using Hangfire.Dashboard.BasicAuthorization;
+using Hangfire.MemoryStorage;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DeUrgenta.RecurringJobs
+{
+    public static class HangfireExtensions
+    {
+        public static void AddHangfireServices(this IServiceCollection services)
+        {
+            // Add Hangfire services.
+            services.AddHangfire(configuration => configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseMemoryStorage()
+                );
+
+            // Add the processing server as IHostedService
+            services.AddHangfireServer();
+        }
+
+        public static void UseAuthenticatedHangfireDashboard(this IApplicationBuilder app, IConfiguration configuration)
+        {
+            var filter = new BasicAuthAuthorizationFilter(
+                        new BasicAuthAuthorizationFilterOptions
+                        {
+                            // Require secure connection for dashboard
+                            RequireSsl = true,
+                            // Case sensitive login checking
+                            LoginCaseSensitive = true,
+                            // Users
+                            Users = new[]
+                            {
+                                new BasicAuthAuthorizationUser
+                                {
+                                    Login = configuration["Hangfire:Dashboard:Username"],
+                                    // Password as plain text, SHA1 will be used
+                                    PasswordClear = configuration["Hangfire:Dashboard:Password"]
+                                }
+                            }
+                        });
+
+            var options = new DashboardOptions
+            {
+                DashboardTitle = "De Urgenta Hangfire Dashboard",
+                Authorization = new IDashboardAuthorizationFilter[]
+                {
+                    filter
+                }
+            };
+            app.UseHangfireDashboard(options: options);
+        }
+    }
+}
