cbac535b613600ef550e225beed6697c1946aa47
Alex Stefanescu <s.alexandra.stefanescu@gmail.com>
2021-04-02 10:40:47 +0300

    Fix bash script for CI

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,11 +1,5 @@
-#! /usr/bin/env sh
-set -x
-
-DEPLOY_DIR=$(dirname "$0")
-SOURCE_DIR="${DEPLOY_DIR}/../"
-
-cd $SOURCE_DIR \
-    && git pull \
-    && docker-compose down \
-    && docker-compose build \
-    && docker-compose up -d
+cd /root/de-urgenta-backend
+git pull
+docker-compose build
+docker-compose down
+docker-compose up -d
