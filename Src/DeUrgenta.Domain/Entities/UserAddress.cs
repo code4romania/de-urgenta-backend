b4c4603b60c1707cfd0107ca69fb7bee435707a3
Ion Dormenco <idormenco@gmail.com>
2021-04-02 16:09:36 +0300

    Add address category column

--- Src/DeUrgenta.Domain/Entities/UserAddress.cs
+++ Src/DeUrgenta.Domain/Entities/UserAddress.cs
@@ -8,6 +8,7 @@ namespace DeUrgenta.Domain.Entities
         [Key]
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public string Category { get; set; }
         public decimal Latitude { get; set; }
         public decimal Longitude { get; set; }
     }
