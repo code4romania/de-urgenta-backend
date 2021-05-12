f268658360fcfa2faf7e692a581a928a1cbc4f01
Ana-Maria Ciufu <ciufu.ana.maria@gmail.com>
2021-05-12 21:34:30 +0300

    removed unused namespaces

--- Src/DeUrgenta.Api/Startup.cs
+++ Src/DeUrgenta.Api/Startup.cs
@@ -18,8 +18,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using DeUrgenta.Backpack.Api;
-using System.Collections.Generic;
 
 
 namespace DeUrgenta.Api
