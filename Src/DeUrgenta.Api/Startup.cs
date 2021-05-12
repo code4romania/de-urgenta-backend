b0a53a36521f671e33ab55425d97ae57bbd71ea3
Ana-Maria Ciufu <ciufu.ana.maria@gmail.com>
2021-05-12 21:35:29 +0300

    small fix

--- Src/DeUrgenta.Api/Startup.cs
+++ Src/DeUrgenta.Api/Startup.cs
@@ -19,7 +19,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-
 namespace DeUrgenta.Api
 {
     public class Startup
