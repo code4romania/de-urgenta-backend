b6e4f02c4938d31f539a457303d3b6004f5b7def
Ion Dormenco <idormenco@gmail.com>
2021-10-08 11:45:02 +0300

    Add health checks

---
+++ Src/DeUrgenta.RecurringJobs/HealthChecksExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using DeUrgenta.Emailing.Service;
+
+namespace DeUrgenta.RecurringJobs
+{
+    public static class HealthChecksExtensions
+    {
+        public static IServiceCollection SetupHealthChecks(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddHealthChecks()
+                .AddNpgSql(configuration.GetConnectionString("DbConnectionString"), name: "de-urgenta-db")
+                .AddNpgSql(configuration.GetConnectionString("JobsConnectionString"), name: "jobs-db")
+                .AddEmailingService(configuration)
+                .AddHangfire(setup =>
+                {
+                    setup.MaximumJobsFailed = 1;
+                    setup.MinimumAvailableServers = 1;
+                });
+
+            services
+                .AddHealthChecksUI()
+                .AddInMemoryStorage();
+
+            return services;
+        }
+    }
+}
