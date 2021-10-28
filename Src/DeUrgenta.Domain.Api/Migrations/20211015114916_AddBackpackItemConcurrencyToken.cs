76a00bf88de39deff38803ceb1313243b355509e
Vlad Cuciureanu <vlad.c.cuciureanu@gmail.com>
2021-10-28 15:52:16 +0300

    Added a version field to backpack items which is used as a make-shift concurrency token. (#131)

---
+++ Src/DeUrgenta.Domain.Api/Migrations/20211015114916_AddBackpackItemConcurrencyToken.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DeUrgenta.Domain.Migrations
+{
+    public partial class AddBackpackItemConcurrencyToken : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Version",
+                table: "BackpackItems",
+                type: "numeric(20,0)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "BackpackItems");
+        }
+    }
+}
