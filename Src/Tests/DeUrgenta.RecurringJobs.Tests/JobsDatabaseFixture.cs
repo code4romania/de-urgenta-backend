ca7044e9c7466c9541e803704522cc93fbb54727
Iulia Dormenco <iuliaolteanu12@gmail.com>
2022-10-30 15:13:59 +0200

    remove respawn from recurring jobs proj

--- Src/Tests/DeUrgenta.RecurringJobs.Tests/JobsDatabaseFixture.cs
+++ Src/Tests/DeUrgenta.RecurringJobs.Tests/JobsDatabaseFixture.cs
@@ -1,64 +1,22 @@
-﻿using System.Threading.Tasks;
-using DeUrgenta.Domain.RecurringJobs;
+﻿using DeUrgenta.Domain.RecurringJobs;
 using DeUrgenta.Tests.Helpers;
 using Microsoft.EntityFrameworkCore;
-using Npgsql;
-using Respawn;
-using Xunit;
 
 namespace DeUrgenta.RecurringJobs.Tests
 {
-    public class JobsDatabaseFixture : DatabaseFixture, IAsyncLifetime
+    public class JobsDatabaseFixture : DatabaseFixture
     {
-        private readonly JobsTestConfig _testConfig;
-        private readonly Checkpoint _emptyJobsDatabaseCheckpoint;
-
         public JobsContext JobsContext;
 
         public JobsDatabaseFixture()
         {
-            _testConfig = new JobsTestConfig();
+            var testConfig = new JobsTestConfig();
             var optionsBuilder = new DbContextOptionsBuilder<JobsContext>();
-            optionsBuilder.UseNpgsql(_testConfig.JobsConnectionString);
-            if (_testConfig.UseDbCheckpoint)
-            {
-                _emptyJobsDatabaseCheckpoint = new Checkpoint
-                {
-                    SchemasToInclude = new[] { "jobs" },
-                    TablesToIgnore = new[] { "__EFMigrationsHistory" },
-                    DbAdapter = DbAdapter.Postgres
-                };
-            }
-
+            optionsBuilder.UseNpgsql(testConfig.JobsConnectionString);
+          
             // Create instance of you application's DbContext
             JobsContext = new JobsContext(optionsBuilder.Options);
             JobsContext.Database.Migrate();
-        }
-
-        public async Task InitializeAsync()
-        {
-            await base.InitializeAsync();
-
-            if (!_testConfig.UseDbCheckpoint)
-                return;
-
-            await using var conn = new NpgsqlConnection(_testConfig.JobsConnectionString);
-            await conn.OpenAsync();
-
-            await _emptyJobsDatabaseCheckpoint.Reset(conn);
-        }
-
-        public async Task DisposeAsync()
-        {
-            await base.DisposeAsync();
-
-            if (!_testConfig.UseDbCheckpoint)
-                return;
-
-            await using var conn = new NpgsqlConnection(_testConfig.JobsConnectionString);
-            await conn.OpenAsync();
-
-            await _emptyJobsDatabaseCheckpoint.Reset(conn);
         }
     }
 }
